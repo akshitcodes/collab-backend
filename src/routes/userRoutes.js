5fada1101b1cf5e0dff19aea2cd050777718e168
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2025-07-19 02:28:07 +0530

    add oauth

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser,joinCollab, leaveCollab } from "../controllers/userController.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
+import passport from "passport";
 
 const router = Router();
 router.post('/register',registerUser);
@@ -8,4 +9,15 @@ router.post('/login',loginUser);
 router.post('/logout', authenticateToken,logoutUser);
 router.post('/join-collab', authenticateToken, joinCollab);
 router.post('/:collab_id/leave',authenticateToken,leaveCollab)
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
+
 export default router;  

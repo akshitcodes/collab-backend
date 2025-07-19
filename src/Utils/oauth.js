3bf8cea3439beb3973a87fad4c5926ee1343f413
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2025-07-20 04:26:04 +0530

    change url to env

--- src/Utils/oauth.js
+++ src/Utils/oauth.js
@@ -24,5 +24,5 @@ const handleGoogleLogin=async (accessToken, refreshToken, profile, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/users/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },handleGoogleLogin));

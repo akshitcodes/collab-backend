e86aef311d9e83cb80e240a5dcf62164c80f1d80
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2025-07-20 03:17:42 +0530

    fix import

--- src/Utils/oauth.js
+++ src/Utils/oauth.js
@@ -1,7 +1,7 @@
 
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { pool } from '../DB/db';
+import { pool } from '../DB/db.js';
 
 console.log("Registering Google OAuth strategy");
 const handleGoogleLogin=async (accessToken, refreshToken, profile, done) => {

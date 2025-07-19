a2d5cc028a973f0b54ccc675e6f2fa19ac35b90b
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2025-07-20 03:26:58 +0530

    await db query

--- src/Utils/oauth.js
+++ src/Utils/oauth.js
@@ -7,7 +7,8 @@ console.log("Registering Google OAuth strategy");
 const handleGoogleLogin=async (accessToken, refreshToken, profile, done) => {
   // Save user to DB, etc.
   try{
-    const user=pool.query('SELECT * FROM users WHERE email = $1', [profile.emails[0].value]);
+    const user=await pool.query('SELECT * FROM users WHERE email = $1', [profile.emails[0].value]);
+    console.log('Google profile:', profile);
   if(user.rows.length===0){
     const newUser=pool.query('INSERT INTO users (email) VALUES ($1) RETURNING *', [profile.emails[0].value]);
     return done(null, newUser);

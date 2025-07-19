36390a22b32bb46cf23e6291bd58e117489a05f8
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2025-07-20 02:06:29 +0530

    update Google OAuth callback URL and add logging

--- src/Utils/oauth.js
+++ src/Utils/oauth.js
@@ -2,10 +2,12 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
+console.log("Registering Google OAuth strategy");
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/users/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
   // Save user to DB, etc.
   return done(null, profile);
